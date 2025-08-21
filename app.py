e53f27d663a22f3b357b646dcfbb9f81956de7f9
furkhan-cloud <mohdfurkhan7755@gmail.com>
2025-08-21 19:03:13 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,146 @@
+S=10
+A=9
+B=8
+C=7
+D=6
+E=5
+
+
+
+#subject 1
+sub1=input("Enter your grade From : ").capitalize()
+if sub1=='S':
+     sub1=10
+elif sub1=='A':
+     sub1=9     
+elif sub1=='B':
+     sub1=8
+elif sub1=='C':
+     sub1=7
+elif sub1=='D':
+     sub1=6
+elif sub1=='E':
+     sub1=5
+else :
+     print("Invalid Grade")   
+
+#subject 2
+sub2=input("Enter your grade From : ").capitalize()
+if sub2=='S':
+     sub2=10
+elif sub2=='A':
+     sub2=9     
+elif sub2=='B':
+     sub2=8
+elif sub1=='C':
+     sub2=7
+elif sub2=='D':
+     sub2=6
+elif sub2=='E':
+     sub2=5
+else :
+     print("Invalid Grade")   
+
+#subject 3
+sub3=input("Enter your grade From : ").capitalize()
+if sub3=='S':
+     sub3=10
+elif sub3=='A':
+     sub3=9     
+elif sub3=='B':
+     sub1=8
+elif sub3=='C':
+     sub3=7
+elif sub3=='D':
+     sub3=6
+elif sub3=='E':
+     sub3=5
+else :
+     print("Invalid Grade")   
+
+#subject 4
+sub4=input("Enter your grade From : ").capitalize()
+if sub4=='S':
+     sub4=10
+elif sub4=='A':
+     sub4=9     
+elif sub4=='B':
+     sub4=8
+elif sub4=='C':
+     sub4=7
+elif sub4=='D':
+     sub4=6
+elif sub4=='E':
+     sub4=5
+else :
+     print("Invalid Grade")   
+
+#subject 5
+sub5=input("Enter your grade From : ").capitalize()
+if sub5=='S':
+     sub5=10
+elif sub5=='A':
+     sub5=9     
+elif sub5=='B':
+     sub5=8
+elif sub5=='C':
+     sub5=7
+elif sub5=='D':
+     sub5=6
+elif sub5=='E':
+     sub5=5
+else :
+     print("Invalid Grade")   
+
+#lab1
+lab1=input("Enter your grade From : ").capitalize()
+if lab1 == "S":
+    lab1=10 
+elif lab1 == "A":
+    lab1=9
+elif lab1 == "B":
+    lab1=8
+elif lab1 == "C":
+    lab1=7
+elif lab1 == "D":
+    lab1=6
+elif lab1 == "E":
+    lab1=5
+else :
+     print("Invalid Grade")   
+        
+#lab2
+lab2=input("Enter your grade From : ").capitalize()
+if lab2 == "S":
+    lab2=10 
+elif lab2 == "A":
+    lab2=9
+elif lab2 == "B":
+    lab2=8
+elif lab2 == "C":
+    lab2=7
+elif lab2 == "D":
+    lab2=6
+elif lab2 == "E":
+    lab2=5
+else :
+     print("Invalid Grade")   
+        
+#lab3
+lab3=input("Enter your grade From : ").capitalize()
+if lab3 == "S":
+    lab3=10 
+elif lab3 == "A":
+    lab3=9
+elif lab3 == "B":
+    lab3=8
+elif lab3 == "C":
+    lab3=7
+elif lab3 == "D":
+    lab3=6
+elif lab3 == "E":
+    lab3=5
+else :
+     print("Invalid Grade")   
+        
+print(f"Your Gpa = {((sub1*4.0)+(sub2*4.0)+(sub3*4.0)+(sub4*4.0)+(sub5*4.0)+(lab1*2.0)+(lab2*2.0)+(lab3*2.0))/24}")
